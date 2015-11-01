da737c389a76d0c7770f4eef6683df2acbdcb19c
Kwaku Twumasi Afriyie <kwaku.twumasi@quakearts.com>
2015-11-01 05:03:18 +0000

    New ajax handler qaboot.overlayHandler

--- Quakearts Webtools Jboss61/boot-src/com/quakearts/webapp/facelets/bootstrap/beans/LoaderBean.java
+++ Quakearts Webtools Jboss61/boot-src/com/quakearts/webapp/facelets/bootstrap/beans/LoaderBean.java
@@ -3,6 +3,8 @@ package com.quakearts.webapp.facelets.bootstrap.beans;
 public class LoaderBean {
 	private static final String MAINAJAXLOADER = "qaboot.mainLoaderHandler";
 	private static final String MINIAJAXLOADER = "qaboot.miniLoaderHandler";
+	private static final String OVERLAYAJAXLOADER = "qaboot.overlayHandler";
+	
 	
 	public String getMainAjaxLoader() {
 		return MAINAJAXLOADER;
@@ -10,5 +12,9 @@ public class LoaderBean {
 	
 	public String getMiniAjaxLoader() {
 		return MINIAJAXLOADER;
+	}
+	
+	public String getOverlayAjaxLoader() {
+		return OVERLAYAJAXLOADER;
 	}
 }
--- Quakearts Webtools Jboss61/boot-src/com/quakearts/webapp/facelets/bootstrap/renderers/BootAjaxLoaderScriptRenderer.java
+++ Quakearts Webtools Jboss61/boot-src/com/quakearts/webapp/facelets/bootstrap/renderers/BootAjaxLoaderScriptRenderer.java
@@ -36,11 +36,63 @@ public class BootAjaxLoaderScriptRenderer extends HtmlBasicRenderer {
 			"    }\r\n" + 
 			"}\r\n" + 
 			"\r\n" + 
+			"qaboot.overlayHandler = function(data){\r\n" + 
+			"    switch (data.status) {\r\n" + 
+			"		case \"begin\":\r\n" + 
+			"			var idobj = $(qaboot.escape(\"#\" + data.source.id));\r\n" + 
+			"			var idparent = idobj.parent();\r\n" + 
+			"			while (idparent.parent().length > 0) {\r\n" + 
+			"				if (idparent.hasClass(\"ajax-container\")) {\r\n" + 
+			"					$('body').append(\"<div id='\"+data.source.id+\"_overlay' class='collapse'>\"+\r\n" + 
+			"							\"<img src='$overlayimg' border='0' $overlayimagestyle/>\"+\r\n" + 
+			"								\"</div>\"); \r\n" + 
+			"					var obj= $(qaboot.escape(\"#\"+data.source.id+\"_overlay\"));\r\n" + 
+			"					obj.css({\r\n" + 
+			"						position : 'absolute',\r\n" + 
+			"						top : idparent.offset().top,\r\n" + 
+			"						left : idparent.offset().left,\r\n" + 
+			"						width : idparent.css('width'),\r\n" + 
+			"						height : idparent.css('height')\r\n" + 
+			"					});\r\n" + 
+			"					obj.removeClass(\"collapse\").addClass(\"overlay\");\r\n" + 
+			"					obj.animate({\r\n" + 
+			"						opacity : 0.8\r\n" + 
+			"					}, 1000, function() {\r\n" + 
+			"\r\n" + 
+			"					});\r\n" + 
+			"\r\n" + 
+			"					break;\r\n" + 
+			"				}\r\n" + 
+			"				idparent = idparent.parent();\r\n" + 
+			"			}\r\n" + 
+			"			break;\r\n" + 
+			"		case \"complete\":\r\n" + 
+			"			var obj= $(qaboot.escape(\"#\"+data.source.id+\"_overlay\"));\r\n" + 
+			"			if(obj.length>0){					\r\n" + 
+			"				obj.animate(\r\n" + 
+			"					{\r\n" + 
+			"						opacity : 0.0\r\n" + 
+			"					},\r\n" + 
+			"					500,\r\n" + 
+			"					function() {\r\n" + 
+			"						var obj= $(qaboot.escape(\"#\"+data.source.id+\"_overlay\"));\r\n" + 
+			"						obj.removeClass(\"overlay\")\r\n" + 
+			"								.addClass(\"collapse\");\r\n" + 
+			"						obj.remove();\r\n" + 
+			"					});\r\n" + 
+			"			}\r\n" + 
+			"			break;\r\n" + 
+			"		case \"success\":\r\n" + 
+			"			break;\r\n" + 
+			"		default:\r\n" + 
+			"		}\r\n" + 
+			"	}\r\n"+
+			"\r\n" + 
 			"qaboot.miniLoaderHandler = function(data){\r\n" + 
 			"    switch(data.status){\r\n" + 
 			"        case \"begin\":\r\n" + 
 			"            var parent = $(qaboot.escape(\"#\"+data.source.id)).parent();\r\n" + 
-			"            parent.append(\"<img src='$img' id='\"+data.source.id+\"_img' $style/>\");\r\n" + 
+			"            parent.append(\"<img src='$miniimg' id='\"+data.source.id+\"_img' $miniimagestyle/>\");\r\n" + 
 			"            break;\r\n" + 
 			"        case \"complete\":\r\n" + 
 			"            $(qaboot.escape(\"#\"+data.source.id+\"_img\")).remove();\r\n" + 
@@ -49,7 +101,7 @@ public class BootAjaxLoaderScriptRenderer extends HtmlBasicRenderer {
 			"            break;\r\n" + 
 			"        default:\r\n" + 
 			"    }\r\n" + 
-			"}";
+			"}\r\n";
 	
 	@Override
 	public void encodeBegin(FacesContext context, UIComponent component)
@@ -75,10 +127,13 @@ public class BootAjaxLoaderScriptRenderer extends HtmlBasicRenderer {
 			String miniimagestyle = loaderComponent.get("miniimagestyle");
 			String startTimeout = loaderComponent.get("startTimeout");
 			String endTimeout = loaderComponent.get("endTimeout");
+			String overlayimagestyle = loaderComponent.get("overlayimagestyle");
 			
 			writer.write(LOADERSCRIPT.replace("$ajaxdiv", loaderComponent.getClientId(context))
-					.replace("$img", loaderComponent.get("miniloaderimage"))
-					.replace("$style", miniimagestyle!=null?("style=\""+miniimagestyle+"\" "):"")
+					.replace("$miniimg", loaderComponent.get("miniloaderimage"))
+					.replace("$overlayimg", loaderComponent.get("overlayloaderimage"))
+					.replace("$miniimagestyle", miniimagestyle!=null?("style=\""+miniimagestyle+"\" "):"")
+					.replace("$overlayimagestyle", overlayimagestyle!=null?("style=\""+overlayimagestyle+"\" "):"")
 					.replace("$stime", startTimeout)
 					.replace("$etime", endTimeout));
 			writer.endElement("script");
