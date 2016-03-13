6ae164f97b4b0ab071bf0c35e74e8bfd12d54019
Kwaku Twumasi-Afriyie <ktwumasi@smsgh.com>
2016-03-13 14:38:45 +0000

    MetaRule to passthrough "data-" attributes for Bootstrap tags

---
+++ Webtools Main/boot-src/com/quakearts/webapp/facelets/bootstrap/handlers/HTML5DataRule.java
@@ -0,0 +1,71 @@
+package com.quakearts.webapp.facelets.bootstrap.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.component.UIComponent;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.MetaRule;
+import javax.faces.view.facelets.Metadata;
+import javax.faces.view.facelets.MetadataTarget;
+import javax.faces.view.facelets.TagAttribute;
+
+public class HTML5DataRule extends MetaRule {
+
+	public static final HTML5DataRule Instance = new HTML5DataRule();
+	public static final String HTML5DATAATTRIBUTES = "com.quakearts.bootstrap.DATAATTRIBUTES";
+	
+	private HTML5DataRule() {
+	}
+	
+	final static class HTML5MetaData extends Metadata {
+		String name;
+		String value;
+		TagAttribute attr;
+		Class<?> type;
+
+		public HTML5MetaData(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
+
+		public HTML5MetaData(String name, TagAttribute attr, Class<?> type) {
+			this.name = name;
+			this.attr = attr;
+			this.type = type;
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public void applyMetadata(FaceletContext ctx, Object instance) {
+			List<String> html5passthru = (List<String>) ((UIComponent)instance).getAttributes().get(HTML5DATAATTRIBUTES);
+			if(html5passthru==null){
+				html5passthru = new ArrayList<>();
+				((UIComponent)instance).getAttributes().put(HTML5DATAATTRIBUTES, html5passthru);
+			}
+			html5passthru.add(name);
+			if(value!=null){
+				((UIComponent)instance).getAttributes().put(name, value);
+			} else {
+				((UIComponent)instance).setValueExpression(name, attr.getValueExpression(ctx, type));
+			}			
+		}
+	}
+	
+	@Override
+	public Metadata applyRule(String name, TagAttribute attribute, MetadataTarget meta) {
+		if(name.startsWith("data-")){
+			if(attribute.isLiteral()){
+				return new HTML5MetaData(name, attribute.getValue());
+			} else {
+				Class<?> type = meta.getPropertyType(name);
+                if (type == null) {
+                    type = Object.class;
+                }
+                return new HTML5MetaData(name, attribute, type);
+			}
+		}
+		return null;
+	}
+
+}
