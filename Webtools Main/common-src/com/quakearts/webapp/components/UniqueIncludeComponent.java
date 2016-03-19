c7567b94075b5973490adb9b7cd31c2c6f7deeb1
Kwaku Twumasi-Afriyie <ktwumasi@smsgh.com>
2016-03-19 10:10:38 +0000

    Minified qaboot.min.css, added unique component, and minified js

---
+++ Webtools Main/common-src/com/quakearts/webapp/components/UniqueIncludeComponent.java
@@ -0,0 +1,62 @@
+package com.quakearts.webapp.components;
+
+import java.util.Collection;
+
+import javax.faces.component.NamingContainer;
+import javax.faces.component.UIComponentBase;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.UniqueIdVendor;
+import javax.faces.component.visit.VisitCallback;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitResult;
+import javax.faces.context.FacesContext;
+
+public class UniqueIncludeComponent extends UIComponentBase implements NamingContainer, UniqueIdVendor {
+
+	public static final String COMPONENT_FAMILY = "com.quakearts.webapp.components.unique";
+
+	enum PropertyKeys {
+		LASTID
+	}
+
+	@Override
+	public String createUniqueId(FacesContext context, String seed) {
+		Integer i = (Integer) getStateHelper().get(PropertyKeys.LASTID);
+		int lastId = ((i != null) ? i : 0);
+		getStateHelper().put(PropertyKeys.LASTID, ++lastId);
+		return UIViewRoot.UNIQUE_ID_PREFIX + (seed == null ? lastId : seed);
+	}
+
+	@Override
+	public String getFamily() {
+		return COMPONENT_FAMILY;
+	}
+	
+	@Override
+	public String getRendererType() {
+		return null;
+	}
+
+	@Override
+	public boolean visitTree(VisitContext context, VisitCallback callback) {
+		Collection<String> idsToVisit = context.getSubtreeIdsToVisit(this);
+		assert (idsToVisit != null);
+
+		if (!idsToVisit.isEmpty()) {
+			return super.visitTree(context, callback);
+		}
+
+		if (isVisitable(context)) {
+			FacesContext facesContext = context.getFacesContext();
+			pushComponentToEL(facesContext, null);
+
+			try {
+				VisitResult result = context.invokeVisitCallback(this, callback);
+				return (result == VisitResult.COMPLETE);
+			} finally {
+				popComponentFromEL(facesContext);
+			}
+		}
+		return false;
+	}
+}
