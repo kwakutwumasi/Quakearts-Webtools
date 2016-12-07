b404e6db481bd3760464792a7eb7246c50c32dd9
Kwaku Twumasi-Afriyie <ktwumasi@smsgh.com>
2016-12-07 21:09:27 +0000

    Bug fixes and updates

--- qa-common/src/main/java/com/quakearts/webapp/codegeneration/annotations/CodeGeneratorProperties.java
+++ qa-common/src/main/java/com/quakearts/webapp/codegeneration/annotations/CodeGeneratorProperties.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.TYPE})
+@Target({ElementType.FIELD,ElementType.TYPE,ElementType.METHOD})
 public @interface CodeGeneratorProperties {
 	CodeGeneratorProperty[] value();
 }
