29ec12d2e95370b0ee0000fddf61f0236f7ce21a
Kwaku Twumasi-Afriyie <kwakutwumasi-afriyie@quakearts.com>
2018-04-10 21:36:00 +0000

    Removal of discontinued project

--- qa-workflow/src/main/java/com/quakearts/workflowapp/jbpm/audit/converter/DataConverter.java
+++
@@ -1,8 +0,0 @@
-package com.quakearts.workflowapp.jbpm.audit.converter;
-
-import java.util.Properties;
-
-public interface DataConverter {
-    public String convert(Object obj);
-    public void initialize(Properties props);
-}
