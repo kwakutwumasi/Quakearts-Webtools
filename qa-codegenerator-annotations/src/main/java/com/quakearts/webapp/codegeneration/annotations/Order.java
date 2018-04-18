4df11b21f8413d5e8cad69cf7b37fb3b6206e63d
Kwaku Twumasi-Afriyie <kwakutwumasi-afriyie@quakearts.com>
2018-04-18 01:05:17 +0000

    Split from qa-common

---
+++ qa-codegenerator-annotations/src/main/java/com/quakearts/webapp/codegeneration/annotations/Order.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright (C) 2016 Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com> - initial API and implementation
+ ******************************************************************************/
+package com.quakearts.webapp.codegeneration.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD,ElementType.METHOD})
+public @interface Order {
+	int value();
+}
