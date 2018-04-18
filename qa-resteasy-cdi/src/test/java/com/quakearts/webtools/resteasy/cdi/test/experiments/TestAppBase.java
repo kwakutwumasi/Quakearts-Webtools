7f4585a224205cd751277e350c55deb833d4bf69
Kwaku Twumasi-Afriyie <kwakutwumasi-afriyie@quakearts.com>
2018-04-18 02:14:14 +0000

    License header updates, addition of license file

--- qa-resteasy-cdi/src/test/java/com/quakearts/webtools/resteasy/cdi/test/experiments/TestAppBase.java
+++ qa-resteasy-cdi/src/test/java/com/quakearts/webtools/resteasy/cdi/test/experiments/TestAppBase.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 package com.quakearts.webtools.resteasy.cdi.test.experiments;
 
 import javax.inject.Inject;
