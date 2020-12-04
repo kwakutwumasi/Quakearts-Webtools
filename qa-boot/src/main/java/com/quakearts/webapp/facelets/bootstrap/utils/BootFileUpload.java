e054282df35c5f4f3b4b49e8b81fc45d71447980
Kwaku Twumasi-Afriyie <kwaku.twumasi@quakearts.com>
2020-12-04 14:46:54 +0000

    Update: Make BootFileUpload pass InputStream to user
    This prevents large files from being loaded into memory

--- qa-boot/src/main/java/com/quakearts/webapp/facelets/bootstrap/utils/BootFileUpload.java
+++ qa-boot/src/main/java/com/quakearts/webapp/facelets/bootstrap/utils/BootFileUpload.java
@@ -20,25 +20,33 @@ import javax.servlet.http.Part;
 public class BootFileUpload {
 	private String fileName;
 	private byte[] data;
+	private Part filePart;
 	
 	public String getFileName() {
 		return fileName;
 	}
 	
 	public byte[] getData() {
+		if(data == null){
+			data = new byte[(int)filePart.getSize()];
+			try(InputStream is=filePart.getInputStream()) {
+				is.read(data);
+			} catch (IOException e) {
+				LOGGER.warning("Exception of type " + e.getClass().getName()
+						+ " was thrown. Message is " + e.getMessage()
+						+ ". Exception occured whiles loading input file "+fileName);
+			}
+		}
 		return data;
+	}
+	
+	public Part getFilePart() {
+		return filePart;
 	}
 	
 	public BootFileUpload(String fileName, Part filePart) {
 		this.fileName = fileName;
-		data = new byte[(int)filePart.getSize()];
-		try(InputStream is=filePart.getInputStream()) {
-			is.read(data);
-		} catch (IOException e) {
-			LOGGER.warning("Exception of type " + e.getClass().getName()
-					+ " was thrown. Message is " + e.getMessage()
-					+ ". Exception occured whiles loading input file "+fileName);
-		}
+		this.filePart = filePart;
 	}
 		
 }
